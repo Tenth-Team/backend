3b991d45d73c9531c9a684d51f4485ce50ba19fc
Sherif Ragimov <ragimov700@yandex.ru>
2024-02-23 22:55:43 +0400

    added ambassadors app and models

---
+++ backend/ambassadors/choices.py
@@ -0,0 +1,19 @@
+GENDER_CHOICES = [
+    ('М', 'Мужской'),
+    ('Ж', 'Женский'),
+]
+
+STATUS_CHOICES = [
+    ('active', 'Активный'),
+    ('paused', 'На паузе'),
+    ('not_ambassador', 'Не амбассадор'),
+    ('pending', 'Уточняется'),
+]
+
+CLOTHING_SIZES_CHOICES = [
+    ('XS', 'XS'),
+    ('S', 'S'),
+    ('M', 'M'),
+    ('L', 'L'),
+    ('XL', 'XL'),
+]
