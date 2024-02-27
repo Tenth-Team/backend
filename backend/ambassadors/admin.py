10b279798a1b0acc589ee44b7833abba10d07499
Sherif Ragimov <ragimov700@yandex.ru>
2024-02-27 17:05:49 +0400

    config admin for ambassadors

--- backend/ambassadors/admin.py
+++ backend/ambassadors/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from .models import Ambassador, TrainingProgram, AmbassadorGoal
-@admin.register(Ambassador)
-class AmbassadorAdmin(admin.ModelAdmin):
-    pass
+
 
 @admin.register(TrainingProgram)
-class AmbassadorAdmin(admin.ModelAdmin):
-    pass
+class TrainingProgramAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 
 @admin.register(AmbassadorGoal)
+class AmbassadorGoalAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Ambassador)
 class AmbassadorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('full_name', 'gender', 'country',
+                    'city', 'status', 'reg_date')
+    list_filter = ('gender', 'country', 'city', 'status', 'reg_date')
+    search_fields = ('full_name', 'country', 'city', 'email', 'phone_number')
+    filter_horizontal = ('amb_goal',)
