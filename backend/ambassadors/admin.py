53c6c535da27d1de0a7c06281eb218b1df7a971b
Sherif Ragimov <ragimov700@yandex.ru>
2024-02-27 12:15:34 +0400

    integration with yandex forms

--- backend/ambassadors/admin.py
+++ backend/ambassadors/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ambassador, TrainingProgram, AmbassadorGoal
+@admin.register(Ambassador)
+class AmbassadorAdmin(admin.ModelAdmin):
+    pass
 
-# Register your models here.
+@admin.register(TrainingProgram)
+class AmbassadorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AmbassadorGoal)
+class AmbassadorAdmin(admin.ModelAdmin):
+    pass
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+
 from ambassadors.models import Ambassador
 from .serializers import AmbassadorSerializer
+
 
 class AmbassadorViewSet(viewsets.ModelViewSet):
     queryset = Ambassador.objects.all()
