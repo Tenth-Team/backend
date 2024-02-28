f125111316898335d57effc29843f7e3ba9f133d
Mikhail Zelenchuck <zelmisha2@gmail.com>
2024-02-28 11:09:35 +0300

    Update admin.py

--- backend/ambassadors/admin.py
+++ backend/ambassadors/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Content, Ambassador, TrainingProgram, AmbassadorGoal
+from .models import Ambassador, TrainingProgram, AmbassadorGoal
 
 
 @admin.register(TrainingProgram)
