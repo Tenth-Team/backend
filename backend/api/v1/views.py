e2fdbc102c1377e53a417b9781f9417be897839c
qwertttyyy <zelmisha2@gmail.com>
2024-03-01 02:39:36 +0300

    added docs

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, filters
 from ambassadors.models import Ambassador, Content
 from rest_framework.pagination import LimitOffsetPagination
 
-from .filters import StatusFilter
+from .filters import ContentFilter
 from .serializers import AmbassadorSerializer, ContentSerializer
 
 
@@ -17,6 +17,6 @@ class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
     pagination_class = LimitOffsetPagination
-    filterset_class = StatusFilter
+    filterset_class = ContentFilter
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ('full_name',)
