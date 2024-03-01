d2404a52d1ad118f3eebae020e4b73b5c9e466d0
qwertttyyy <zelmisha2@gmail.com>
2024-03-01 22:30:52 +0300

    pep fix

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 
 from .views import AmbassadorViewSet, ContentViewSet
@@ -13,5 +13,9 @@ urlpatterns = [
     # path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(
+        'swagger/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
