da3dbfabec826dcc5da5ee01ba8359cda09110e2
qwertttyyy <zelmisha2@gmail.com>
2024-03-01 22:21:46 +0300

    added docs

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 from rest_framework.routers import DefaultRouter
 
 from .views import AmbassadorViewSet, ContentViewSet
@@ -9,6 +10,8 @@ router.register(r'content', ContentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
+    # path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'djoser',
     'ambassadors.apps.AmbassadorsConfig',
     'users',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -98,4 +99,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
